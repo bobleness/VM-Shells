1f6f277c052a3c30b2c4e359bd151ead43f6c74a
bobleness <105673352+bobleness@users.noreply.github.com>
2022-05-17 09:29:56 -0400

    Update and rename bash_test_shell to bash_test_shell.sh

---
+++ bash_test_shell.sh
@@ -0,0 +1,8 @@
+echo "Hopefully this writes a line of code to download something in Bash on a VM, we'll see how it goes."
+
+#We gonna try for downloading GNU and Cubic in the same .sh file
+
+sudo apt-get update
+sudo apt-get -y install cubicsdr
+
+sudo apt-get -y install gnuradio
